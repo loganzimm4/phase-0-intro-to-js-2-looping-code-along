100c580443d8ad85b50e31c8408528206d43c9b9
loganzimm4 <101672520+loganzimm4@users.noreply.github.com>
2022-03-23 14:28:32 -0400

    commit tree 2306d663520853598a324464c98a496b3bd45553

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
-// Code your solutions in this file
+const cards = ["Guadalupe", "Ollie", "Aki"]
+const thankYou = [];
+
+function writeCards(cards) {
+    for (let i = 0; i < cards.length; i++) {
+        cards[i] = (`Thank you, ${cards[i]}, for the wonderful surprise gift!`);
+        debugger;
+    }
+    return cards;
+}
+writeCards(cards);
+
+function countDown() {
+    let i = 10;
+    while (i >= 0) {
+        console.log(i--);
+    }
+}
